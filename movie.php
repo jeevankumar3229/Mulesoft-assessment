2b088363877dd6622b84f32a6298215aa25bee70
jeevankumar3229 <93439382+jeevankumar3229@users.noreply.github.com>
2022-06-16 13:38:24 +0530

    Update movie.php

--- movie.php
+++ movie.php
@@ -14,7 +14,7 @@
 
              $insert = mysqli_query($conn,$sql);                     //executing query
              echo '<script type="text/javascript">';
-                echo 'alert("stocks added successfully");';
+                echo 'alert("movies added successfully");';
                 echo 'window.location.href="display.php";';
                 echo "</script>";
 ?>
